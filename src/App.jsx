f093d4a229200920d45a1464f2f7cbc42de8afb6
christopher <christopher.fx45@gmail.com>
2023-11-21 00:09:53 +0900

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Box, OrderedList, ListItem  } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
-import {photo} from "./src/photo.jpg"
+import photo from "../src/photo.jpg"
 
 export default function App() {
   const property = {
@@ -17,7 +17,7 @@ export default function App() {
     <Box  w='100%' bgColor='white' border='1px' borderColor='black.200' borderRadius='30px'>
       
       <Box>
-      <Image marginLeft="2%"src= {photo} alt={property.imageAlt } w='40%'></Image>
+      <Image marginLeft="2%"src= {photo} alt={property.imageAlt } w='40%' borderRadius='30px'></Image>
       <OrderedList>
           <Box w= "50%">Languages</Box>
           <ListItem textAlign={'left'}> Japanese</ListItem>
