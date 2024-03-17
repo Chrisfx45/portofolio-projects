f64b283a0eee59341f6f0197f4e50dc65956a110
Christopher Darmawirya <christopher.fx45@gmail.com>
2024-03-17 22:48:55 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Image } from '@chakra-ui/react'
 import photo from "../src/photo.jpg"
 import mail from "../src/envelope.png"
 import ig from "../src/ig.png"
-import linked from "../src/linked.png"
+import linked from "../src/placeholder.png"
 import px from "../src/px.png"
 import wa from "../src/wa.png"
 
@@ -29,54 +29,39 @@ export default function App() {
 
 
   return (
-    <Center>
-  <Box display="flex" boxShadow='dark-lg' minWidth="735" minHeight = "1150"rounded='md' maxW="800"  w='100%'h="100%" bgColor='white' border='1px' borderColor='black.200' borderRadius='30px'>
+  <Center>
+  <Box display="flex" boxShadow='dark-lg' minWidth="800" minHeight = "1060"rounded='md' maxW="800" maxH={"1131"}  w='100%'h="100%" bgColor='white' border='1px' borderColor='black.200' borderRadius='30px'>
 
-      <Box w="30%"  >
-        <Center marginBottom="10%" w= "120.5%" height="28%" borderTopLeftRadius="30px" backgroundColor="darkgray" >
-          <Image  bgColor='white' border='2px' borderColor='blue.300' p = "2%"marginLeft="2%"src= {photo} alt={property.imageAlt } w='80%' borderRadius='50%'></Image>
+      <Box w="35%" borderRight='2px'borderColor='lightgray'>
+        <Center marginBottom="5%" w= "100%" minH={"13.1%"} position={"relative"} borderTopLeftRadius="30px" backgroundColor="lightgray"  >
+
+          <Image position={"absolute"}style={{top: "15px", left: "100px"}} bgColor='white' border='2px' borderColor='lightgray' p = "2%"src= {photo} alt={property.imageAlt } w='60%' borderRadius='50%'></Image>
         </Center>
-        <Center marginBottom="5%" w= "100%">Basic Info</Center>
-        <Box marginLeft= "8%" display='flex' w="100%"  borderBottom='2px' borderColor='blue.300' fontSize ="0.9vw" >
-                 
-            <Box marginLeft="2%" w= "46%" >
-            <OrderedList >
-              <ListItem textAlign={'left'}> Date of Birth</ListItem>
-              <ListItem textAlign={'left'}> Nationality</ListItem>
-              <ListItem textAlign={'left'}> Residence</ListItem>
-            </OrderedList>
-            </Box>
-            <Box marginBottom="5%"  w = "46%">
-            < UnorderedList  styleType="none" >
-              <ListItem textAlign={'right'}>2000/07/15</ListItem>
-              <ListItem textAlign={'right'}>Indonesian</ListItem>
-              <ListItem textAlign={'right'}>Jakarta, Indonesia</ListItem>
-            </UnorderedList >
-            </Box>
-          </Box>
+       
 
 
           
-        <Center w="100%" marginTop = "5%">Contacts</Center>
+        <Center marginLeft="8%" w="92%" marginTop = "21.5%" marginBottom="2%" fontSize = "150%">Contacts</Center>
           <Box >         
-            <Box marginBottom="5%"marginLeft="8%" w= "100%" borderBottom='2px' borderColor='blue.300' >
-            <OrderedList marginBottom="10%" >
-              <ListItem textAlign={'left'}display="flex" fontSize="small">< Image w="10%" src={mail} alt='mail' marginRight="1%"></Image>christopher.fx45@gmail.com</ListItem>
-              <ListItem textAlign={'left'}display="flex" fontSize="small"><Image w="10%" src={wa} alt='wa'marginRight="1%"></Image>+6281 9258 3010</ListItem>
-              <ListItem textAlign={'left'}display="flex" fontSize="small"><Image w="10%" src={linked} alt='ig' marginRight="1%"></Image>Christopher Darmawirya</ListItem>
+            <Box marginLeft="8%" w= "92%" borderBottom='2px' borderColor='lightgray' >
+            <OrderedList marginBottom="5%" >
+              <ListItem textAlign={'left'}display="flex" fontSize="small">< Image w="8%"  height= "3%" src={mail} alt='mail' marginRight="1%"></Image>christopher.fx45@gmail.com</ListItem>
+              <ListItem textAlign={'left'}display="flex" fontSize="small"><Image w="8%"  height= "3%"src={wa} alt='wa'marginRight="1%"></Image>+6281 9258 3010</ListItem>
+              <ListItem textAlign={'left'}display="flex" fontSize="small"><Image w="8%" height= "3%" src={linked} alt='ig' marginRight="1%"></Image>Pelepah Raya WT1 No.22</ListItem>
+              <ListItem textAlign={'left'}display="flex" fontSize="small" marginLeft="9%"> Kelapa Gading, Jakarta</ListItem>
             </OrderedList>
             </Box>
           </Box>
-          <Center marginBottom="5%" w= "100%">Languages</Center>
-          <Box marginLeft= "8%" display='flex' w="100%"  borderBottom='2px' borderColor='blue.300' >         
-            <Box marginLeft="2%" w= "46%"  >
+          <Center marginLeft="8%" marginBottom="5%" w= "92%" fontSize = "150%">Languages</Center>
+          <Box marginLeft= "8%" display='flex' w="92%"  borderBottom='2px' borderColor='lightgray'fontSize = "90%" >         
+            <Box marginLeft="2%" w= "45%"  >
             <OrderedList >
               <ListItem textAlign={'left'}> Indonesian</ListItem>
               <ListItem textAlign={'left'}> English </ListItem>
               <ListItem textAlign={'left'}> Japanese</ListItem>
             </OrderedList>
             </Box>
-            <Box marginBottom="10%"  w = "46%">
+            <Box marginBottom="12%"  w = "45%">
             < UnorderedList  styleType="none" >
               <ListItem textAlign={'right'}>Native</ListItem>
               <ListItem textAlign={'right'}>Fluent </ListItem>
@@ -85,126 +70,122 @@ export default function App() {
             </Box>
           </Box>
           <Box >
-          <Center marginTop="5%" w="100%">Socials</Center>         
-            <Box marginBottom="5%"marginLeft="8%" w= "100%" >
-            <OrderedList marginBottom="10%" >
-              <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small"><Image w="10%" src={px} alt='px'marginRight="1%"></Image>@chestnut_45</ListItem>
-              <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small"><Image w="10%" src={ig} alt='ig' marginRight="1%"></Image>@fx_chris45</ListItem>
+            <Center marginLeft="8%"  marginTop="2%" w="92%" fontSize = "150%">Skills</Center>         
+            <Box marginBottom="10%"marginLeft="8%" w= "92%" borderBottom='2px' borderColor='lightgray'  >
+            <OrderedList marginBottom="10%"  fontSize = "90%" >
+              <ListItem p="2%" textAlign={'left'}display="flex">Front-end Development</ListItem>
+              <ListItem p="2%" textAlign={'left'}display="flex" >Back-end Development</ListItem>
+              <ListItem p="2%" textAlign={'left'}display="flex" >Application Development</ListItem>
+              <ListItem p="2%" textAlign={'left'}display="flex" >Web design</ListItem>              
+              <ListItem p="2%" textAlign={'left'}display="flex" >Data analysis</ListItem>
+              <ListItem p="2%" textAlign={'left'}display="flex" >Programming Languages: Python, Java, JavaScript, TypeScript, MySQL, C, CSS, HTML</ListItem>
+        
             </OrderedList>
             </Box>
-            <Center marginLeft="8%"  marginTop="5%" w="100%" borderBottom='2px' borderColor='blue.300'>Codes and Projects</Center>         
-            <Box marginBottom="20%"marginLeft="8%" w= "100%"  >
+          </Box>
+          <Box >
+            <Center marginLeft="8%"  marginTop="5%" w="92%">Links to codes</Center>         
+            <Box marginBottom="10%"marginLeft="8%" w= "92%">
             <OrderedList marginBottom="10%" >
               <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">Github :</ListItem>
               <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">https://github.com/Chrisfx45</ListItem>
               <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">LeetCode :</ListItem>
               <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">https://leetcode.com/rip_chris45/</ListItem>
-              <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">Vercel :</ListItem>
-              <ListItem p="2%" textAlign={'left'}display="flex" fontSize="small">https://vercel.com/chrisfx45s-projects</ListItem>
             </OrderedList>
             </Box>
           </Box>
-      </Box>
-      <Box w = "70%"  height="100%">
-      <Box borderTopRightRadius="30px" marginLeft="9%" w="91.5%" backgroundColor="Navy" textColor= "white" > 
-        <Box marginLeft = "10%" w="80%" borderBottom="2px"borderColor='white' fontSize= "200%"marginRight="10%" textAlign= "Right">
-          About Me
-        </Box>
-        <Box display="flex" marginLeft = "10%" justifyContent="space-between" fontSize= "90%"marginTop="2%"textAlign="left" paddingBottom = "3%"  marginRight="10%" >
-                A fresh graduate looking for
-                a job as a software developer or doing data analysis. 
 
-                Enjoy tackling difficult coding problems and 
-                implementing them in personal mini projects. Other hobbies include learning
-                many programming languages, developing my personal game using Godot, maintaining PC parts and
-                engaging in many arts activities such as drawing, designning, etc.  
-        </Box>
+
       </Box>
-      <Box  marginLeft="10%" w="90%" height="100%" >
-        <Box marginLeft = "10%" w="80%" fontSize= "200%"marginRight="10%" textAlign= "Right"  borderBottom='2px' borderColor='blue.300'>
+      <Box w = "65%"  height="100%">
+      <Box borderTopRightRadius="30px" w="100%" backgroundColor='lightgrey' textColor= "black" minHeight={"14%"} > 
+        <Box marginLeft = "10%" paddingTop={"7.5%"} paddingBottom={"7.5%"}  >
+          <Text fontSize={"150%"}>CHRISTOPHER AGAM DARMAWIRYA</Text>
+
+          <Text  fontSize={"100%"}>
+            Software Engineer
+          </Text>
+        </Box>
+
+      </Box>
+      <Box  w="90%"  >
+      <Box marginLeft = "10%" w="80%" marginTop={"8%"} fontSize= "150%"marginRight="10%">
+          Profile
+        </Box>
+        <Box  display="flex" flexGrow={1} marginLeft = "10%" justifyContent="space-between" fontSize= "80%"marginTop="2%"textAlign="left" paddingBottom = "3%"  marginRight="10%" >
+                A software engineer capable of doing data analysis and full stack developtment. Strong willed and creative individual who always strive to grow.
+                Favorite activities include exploring
+                many programming languages, developing my personal game using Godot and tackling difficult coding problems 
+        </Box>
+        <Box  paddingLeft = "10%" w="80%" fontSize= "150%"marginRight="10%" paddingTop={"0.5%"}  borderTop='2px' borderColor='lightgray'>
           Education
          
         </Box >
-          <Grid templateColumns = "repeat(5, 1fr)" gap ={3} marginTop="2%"  marginRight = "5%">
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2}>2019/4 -2023/4<br/>
-                                                    Japan
+          <Grid templateColumns = "repeat(5, 1fr)" gap ={3} marginTop="1%"  marginRight = "5%">
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "80%">2019/4-2023/4
           </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2}>Bachelor of Computer Science at Ritsumeikan University</GridItem>
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2}>2023/10 -2024/2<br/>
-                                                    Indonesia
+          <GridItem colSpan ={3} rowSpan ={2} fontSize = "80%">Bachelor of Computer Science at Ritsumeikan University, Japan</GridItem>
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "80%">2023/10-2024/2
           </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2}>Finished Full Stack Developer Boot Camp at Purwadhika</GridItem>
+          <GridItem colSpan ={3} rowSpan ={2} fontSize = "80%">Finished Full Stack Developer Boot Camp at Purwadhika, Indonesia</GridItem>
 
           </Grid>
       </Box>
-      <Box  marginLeft="10%" w="90%" height="100%" >
-        <Box marginLeft = "10%" w="80%" fontSize= "200%"marginRight="10%" textAlign= "Right"marginTop="3%"   borderBottom='2px' borderColor='blue.300'>
-          Experiences
+      <Box  w="90%"  >
+        <Box paddingLeft = "10%" w="80%" fontSize= "150%" paddingRight="10%" marginTop="3%"   borderTop='2px' borderColor='lightgray'>
+          Experience
         </Box >
           <Grid templateColumns = "repeat(5, 1fr)" gap ={2} marginTop="2%" marginRight = "5%">
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "0.95vw">2023/11-Now<br/>
-                                                   <Text fontSize = "0.85vw"> Remote </Text>
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "90%">2023/11-2024/4<br/>
+                                                   <Text fontSize = "90%"> Freelance</Text>
           </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2} fontSize = "0.95vw">Application Fullstack Developer<br/>
-                                              <Text fontSize = "0.85vw">
-                                              Create an application that can be run with IOS and android devices using React Native frame work.
-                                              Design the application database and its structure.
+          <GridItem colSpan ={3} rowSpan ={2} fontSize = "90%" fontWeight={"bold"}>Application Fullstack Developer<br/>at FDSG <br/>
+                                              <Text fontSize = "90%"  fontWeight={"normal"}>
+
+                                              Created an application that can be run with IOS and android devices using React Native frame work.
+                                              Designed the application database and its structure using MySQL. Designed the backend of the app using Express.
+                                              Used GoogleMap SDK to implement a map feature.
                                               </Text>
                                               </GridItem>
 
 
           </Grid>
       </Box>
-      <Box  marginLeft="10%" w="90%" height="100%" >
-        <Box marginLeft = "10%" w="80%" fontSize= "200%"marginRight="10%" textAlign= "Right"marginTop="3%"   borderBottom='2px' borderColor='blue.300'>
+
+      <Box  w="90%" height="100%" >
+        <Box paddingLeft = "10%" w="80%" fontSize= "150%"marginRight="5%" marginTop="3%">
           Personal Projects
         </Box >
           <Grid templateColumns = "repeat(5, 1fr)" gap ={2} marginTop="2%" marginRight = "5%">
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "0.95vw">2023/12-2024/1<br/>
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "90%">2024/1-2024/2<br/>
+                                              
+                                              </GridItem>
+                                              <GridItem colSpan ={3} rowSpan ={2}fontSize = "90%"  fontWeight={"bold"}>Event Management Website <br/>
+                                              <Text fontSize = "90%"  fontWeight={"normal"}> Created a personal website for an event management using Next.js as the front end and Express for the backend </Text>
+                                              </GridItem>
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "90%">2023/11-2023/12<br/>                                      
+          </GridItem>
+          <GridItem colSpan ={3} rowSpan ={2} fontSize = "90%" fontWeight={"bold"}>Mock Website For Front End Practice <br/>
+          <Text fontSize = "90%"  fontWeight={"normal"}>Designed and created a website with a functional API call from a public domain and deploy it</Text>
+          </GridItem>
+          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "90%" >2023/12-2024/1<br/>
                                                     
           </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2} fontSize = "0.95vw">Portofolio Website <br/>
-                                              <Text fontSize = "0.85vw">Designed a personal portofolio using Chakra and React framework and deploy it to a public domain</Text>
+          <GridItem colSpan ={3} rowSpan ={2} fontSize = "90%" fontWeight={"bold"}>Resume Website <br/>
+                                              <Text fontSize = "90%" fontWeight={"normal"}>Designed a personal portofolio using Chakra and React framework and deploy it to a public domain</Text>
           
           </GridItem>
 
                                               
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "0.95vw">2023/11-2023/12<br/>
-                                                    
-          </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2} fontSize = "0.95vw">Mock Website For Front End Practice <br/>
-          <Text fontSize = "0.85vw">Designed and create a website with a functional API call from a public domain and deploy it</Text>
-          </GridItem>
-          <GridItem colSpan ={2} textAlign ="Right" rowSpan ={2} fontSize = "0.95vw">2024/1-2024/2<br/>
-                                              
-          </GridItem>
-          <GridItem colSpan ={3} rowSpan ={2}fontSize = "0.95vw">Event Management Website <br/>
-          <Text fontSize = "0.85vw"> Created a personal website for an event management using Next.js as the front end and Express for the backend </Text>
-          </GridItem>
+
+
           
 
 
           </Grid>
       </Box>
-      <Box  marginLeft="15%" w="90%" height="100%" >
-        <Box marginLeft = "5%" w="80%" fontSize= "200%"marginRight="10%" textAlign= "Right"marginTop="3%"   borderBottom='2px' borderColor='blue.300'>
-          Skills
-        </Box >
-          <Grid templateColumns = "repeat(4, 1fr)" gap ={1} margin="2%" fontSize = "1vw" marginLeft = "5%" marginRight = "5%" >
-          <GridItem colSpan ={1}fontSize = "0.85vw">JavaScript</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">TypeScript</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">Java</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">HTML</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">CSS</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">Python</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">MYSQL</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">C</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">Godot</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">React</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">Next.js</GridItem>
-          <GridItem colSpan ={1}fontSize = "0.85vw">React Native</GridItem>
-          </Grid>
-      </Box>
+
+
       </Box>
       
      
